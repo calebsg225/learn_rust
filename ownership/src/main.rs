b04234d6c29bfd2a901d3a1c74b3387fd039937f
caleb <calebsg2253284+git@gmail.com>
2024-12-26 23:33:48 -0500

    ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+  let s = String::from("pew");
+  println!("1{s}");
+  let s = take_and_give_back_ownership(s);
+  println!("3{s}");
+
+  let mut s1 = String::from("ssss");
+  println!("1{}", s1);
+  // let l = calc_len(&s1);
+  let l = calc_len_mut(&mut s1);
+  println!("3{}, {}", s1, l);
+
+  let mut s2 = String::from("3rd string, i 0");
+
+  let mut r1 = &mut s2;
+  let r2 = &mut r1;
+
+  println!("{}", r2)
+}
+
+fn take_and_give_back_ownership(new_string: String) -> String {
+  println!("2{new_string}");
+  new_string
+}
+
+fn calc_len(s: &String) -> usize {
+  // s is a reference to the inputed string. It does not have ownership.
+  // references are immutable by default.
+  println!("2{}", s);
+  s.len()
+}
+
+fn calc_len_mut(s: &mut String) -> usize {
+  s.push('a');
+  println!("mut{}", s);
+  s.len()
+}
