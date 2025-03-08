714517f33461cb21d6a6a1a75ea1fef86c6457c5
calebsg225 <calebsg2253284@gmail.com>
2025-03-07 23:12:43 -0500

    Deref Trait, Deref Coersion

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -22,9 +22,18 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
 use crate::List::{Cons, Nil};
 
 fn main() {
+    hello("foo");
+    hello(&String::from("bar"));
+    // deref coersion
+    hello(&MyBox::new(String::from("foobar")));
+
     // Box<T> can be used to store data on the heap instead of the stack.
     // 'b' is a Box<i64>, This Box points to an i64 stored on the heap.
     // When 'b' goes out of scope, both 'b' on the stack and the i64 stored on the heap will be deallocated.
@@ -39,13 +48,31 @@ fn main() {
         )),
     ));
 
+    let xx = 5;
+    // yy is a Box<i32> which points to a copy of xx
+    let yy: Box<i32> = Box::new(xx);
+
+    assert_eq!(5, xx);
+    assert_eq!(5, *yy);
+
+    let xxx = 6;
+    let yyy: MyBox<i32> = MyBox::new(xxx);
+
+    assert_eq!(6, xxx);
+    assert_eq!(6, *yyy);
+
     let x = 5;
     // y holds a reference to x
     let y = &x;
+    // '&' is the reference operator
+    // '*' is the dereference operator
     let z = Box::new(x);
     assert_eq!(5, x);
     // y must be dereferenced in order to be compare to an integer
     assert_eq!(5, *y);
     // z must also be dereferenced as z is a pointer pointing to a value on the heap
     assert_eq!(5, *z);
+
+    // An immutable reference may be coerced into only an immutable reference
+    // A mutable reference may be coerced into either a mutable or immutable reference
 }
